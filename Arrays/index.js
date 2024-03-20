c06c685d88d80fe017d7d396cf5f734e46344e90
Anandamaya Arno <arnoananda@gmail.com>
2024-03-20 12:01:15 +0100

    tuesday class

---
+++ Arrays/index.js
@@ -0,0 +1,47 @@
+const dogsImSitting = ["Coco", "Chanel", "Lila", "Mila"];
+
+const namesOfStudents = [
+  "Diana",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+  "Sablina",
+  "Sam",
+];
+const ageOfStudents = [15, 15, 15];
+const areStudentsSpanish = [false, false, false];
+
+alert(`I have ${namesOfStudents.length} students in my class yayyy`);
+alert(`The names of my students are ${namesOfStudents}`);
+
+for (let i = 0; i < namesOfStudents.length; i++) {
+  namesOfStudents[3];
+  console.log(`In the array index ${i}, the student is ${namesOfStudents[i]}`);
+}
+
+console.log(dogsImSitting);
